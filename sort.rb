22188ce3f5faf3375e4acac4dd78d9fd589f01a8
Patreev Igor <Patreewigor@yandex.ru>
2017-12-12 21:09:37 +0300

    My method sort!

---
+++ sort.rb
@@ -0,0 +1,27 @@
+array = [1,4,2,5,2,1,8,1,2,5,7,3,5,44,3,4]
+
+def quick_sort(mass)
+  array_of_all_the_numbers_one_copy     = []
+  hash_numbers_and_count_numbers        = {}
+  sorted_array                          = []
+
+  (mass.min..mass.max).each do |x|
+    array_of_all_the_numbers_one_copy << x if mass.include?(x)
+  end
+
+  mass.each do |x|
+    if hash_numbers_and_count_numbers.include?(x)
+      hash_numbers_and_count_numbers[x] += 1
+    else
+      hash_numbers_and_count_numbers[x] = 1
+    end
+  end
+
+  array_of_all_the_numbers_one_copy.each do |x|
+    hash_numbers_and_count_numbers[x].times {sorted_array << x}
+  end
+
+  sorted_array
+end
+
+puts quick_sort(array)
